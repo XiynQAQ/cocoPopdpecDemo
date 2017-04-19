34d880994935adf260ce4c6a2a3afc2d82c2ce1e
Djlink-iOS-席亚楠 <xiyn@djlink.cn>
2017-04-19 14:20:21 +0800

    Initial Commit

---
+++ MyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDemo
+//
+//  Created by DingJia on 2017/4/19.
+//  Copyright © 2017年 席亚楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyDemo
+//
+//  Created by DingJia on 2017/4/19.
+//  Copyright © 2017年 席亚楠. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
